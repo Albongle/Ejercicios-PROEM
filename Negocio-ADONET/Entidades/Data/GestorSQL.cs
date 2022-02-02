d1aa6a8782d05ddaa758822a95b9ecdfbd0c7ebc
Albongle <alejandro.bongioanni@gmail.com>
2022-02-02 15:15:51 -0300

    first commit

---
+++ Negocio-ADONET/Entidades/Data/GestorSQL.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Entidades
+{
+    public static class GestorSQL
+    {
+        private static string conexion;
+        static GestorSQL()
+        {
+            GestorSQL.conexion = ConfigurationManager.ConnectionStrings["miConexion"].ConnectionString;
+        }
+
+
+
+        public static Cliente GetClienteById(int id)
+        {
+
+            string sentencia = "SELECT * FROM CLIENTES WHERE id=@id";
+            using (SqlConnection connection = new SqlConnection(GestorSQL.conexion))
+            {
+                SqlCommand command = new SqlCommand(sentencia, connection);
+                command.Parameters.AddWithValue("id", id);
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlDataReader reader = command.ExecuteReader();
+                return reader.Read() == false ? throw new Exception("Cliente no encontrado") : new Cliente(reader.GetInt32(0), reader.GetString(1),
+                    reader.GetString(2), reader.GetString(3), reader.GetInt32(4));
+            }
+        }
+        public static List<Cliente> GetClientes()
+        {
+            List<Cliente> returnCliente = new List<Cliente>();
+
+            string sentencia = "SELECT * FROM CLIENTES";
+            using (SqlConnection connection = new SqlConnection(GestorSQL.conexion))
+            {
+                SqlCommand command = new SqlCommand(sentencia, connection);
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Cliente cliente = new Cliente(reader.GetInt32(0), reader.GetString(1),
+                        reader.GetString(2), reader.GetString(3), reader.GetInt32(4));
+                    returnCliente.Add(cliente);
+                }
+            }
+
+            return returnCliente;
+        }
+
+
+        public static void AddCliente(Cliente cliente)
+        {
+
+            string sentencia = "INSERT INTO CLIENTES (nombre,apellido, direccion, telefono) VALUES (@nombre,@apellido,@direccion,@telefono)"; ;
+            using (SqlConnection connection = new SqlConnection(GestorSQL.conexion))
+            {
+                SqlCommand command = new SqlCommand(sentencia, connection);
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                command.Parameters.AddWithValue("nombre", cliente.Nombre);
+                command.Parameters.AddWithValue("apellido", cliente.Apellido);
+                command.Parameters.AddWithValue("direccion", cliente.Direccion);
+                command.Parameters.AddWithValue("telefono", cliente.Telefono);
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+        public static bool EditCliente(Cliente cliente)
+        {
+
+            if (cliente is not null)
+            {
+
+                string sentencia = "UPDATE CLIENTES SET nombre=@nombre," +
+                    " apellido=@apellido, direccion=@direccion, telefono=@telefono" +
+                    " WHERE id=@id";
+                using (SqlConnection connection = new SqlConnection(GestorSQL.conexion))
+                {
+                    SqlCommand command = new SqlCommand(sentencia, connection);
+                    if (connection.State != System.Data.ConnectionState.Open)
+                    {
+                        connection.Open();
+                    }
+                    command.Parameters.AddWithValue("id", cliente.Id);
+                    command.Parameters.AddWithValue("nombre", cliente.Nombre);
+                    command.Parameters.AddWithValue("apellido", cliente.Apellido);
+                    command.Parameters.AddWithValue("direccion", cliente.Direccion);
+                    command.Parameters.AddWithValue("telefono", cliente.Telefono);
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+
+
+            }
+
+            throw new Exception("No se recibio el cliente a Editar");
+        }
+    }
+}
---
+++ Negocio-ADONET/Entidades/Models/Negocio.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Negocio
+    {
+        
+        private string nombre;
+     
+        public Negocio(string nombre)
+        {
+            this.nombre = nombre;   
+        }
+
+        public List<Cliente> Clientes { get => GestorSQL.GetClientes(); }
+        public string Nombre { get => this.nombre; set => this.nombre = value; }
+
+        public static Negocio operator +(Negocio negocio, Cliente cliente)
+        {
+            
+            try
+            {
+                GestorSQL.AddCliente(cliente);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("No se agrego el cliente", ex);
+            }  
+            return negocio; 
+        }
+        public static Negocio operator |(Negocio negocio, Cliente cliente)
+        {
+            try
+            {
+                GestorSQL.EditCliente(cliente);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se modifico el cliente", ex);
+            }
+
+            return negocio;
+        }
+
+        public Cliente this [int id]
+        {
+            get
+            {
+                if (id > 0)
+                {
+                    return GestorSQL.GetClienteById(id);
+                }
+                else
+                {
+                    throw new Exception("Id invalido o inexistente");
+                }
+            }
+        }
+
+        
+    }
+}
